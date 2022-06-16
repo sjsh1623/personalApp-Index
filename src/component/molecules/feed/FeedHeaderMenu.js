3831e1d8f430b8f2e5414f1b3827a9e75f6b0803
임석현 <sjsh1623@flow.team>
2022-06-16 22:49:40 +0900

    Add Library for bottom menu

--- src/component/molecules/feed/FeedHeaderMenu.js
+++ src/component/molecules/feed/FeedHeaderMenu.js
@@ -3,11 +3,11 @@ import {View, Text} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const FeedHeaderMenu = () => {
-    const BottomSheetRef = useRef();
+
     return (
         <View style={{flexDirection: 'row', alignItems: "center"}}>
             <Icon name="ellipsis-vertical" size={21} onPress={() => {
-                BottomSheetRef.current.show()
+
             }} color="black" style={{
                 alignItems: 'flex-end',
             }}/>
