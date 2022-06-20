0bc49f50b190d4a5b995e15b26c742e9eb3ab7e9
임석현 <sjsh1623@flow.team>
2022-06-21 00:00:42 +0900

    - Background Dimmed Working now

--- src/screens/BlockScreen.js
+++ src/screens/BlockScreen.js
@@ -2,41 +2,44 @@ import React, {useState, useContext} from "react";
 import {Animated} from "react-native";
 import {Context} from "@context";
 
-export const BlockScreen = (props) => {
-    const opacity = useState(new Animated.Value(0))[0];
+
+export const BlockScreen = () => {
     const {
         state: {
-            dimmedScreen: {display},
+            dimmedScreen: {visible, display},
         }
     } = useContext(Context);
-    console.log(display)
 
+    const opacity = useState(new Animated.Value(0))[0];
     const fadeIn = () => {
         Animated.timing(opacity, {
             toValue: 0.4,
-            duration: 1000,
-            useNativeDriver: true
-        }).start();
-    }
-    const fadeOut = () => {
-        Animated.timing(opacity, {
-            toValue: 0,
-            duration: 1000,
+            duration: 300,
             useNativeDriver: true
         }).start();
     }
 
+    const fadeOut = () => {
+        console.log('test')
+        Animated.timing(opacity, {
+            toValue: 0,
+            duration: 200,
+            useNativeDriver: true
+        }).start();
+    }
+
+    visible ? fadeIn() : fadeOut();
+
     return (
         <Animated.View style={{
+            display: display,
+            opacity: opacity,
             position: 'absolute',
             height: '100%',
             width: '100%',
             backgroundColor: 'black',
-            display: 'none',
-            opacity: opacity,
         }}>
         </Animated.View>
-
     )
 };
 
