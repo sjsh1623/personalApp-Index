b8279be261a5d8422569e5e240d4a9e766b333cf
임석현 <sjsh1623@flow.team>
2022-07-05 22:42:40 +0900

    - Use State for text input height - Set more options for text input

--- src/screens/main/chat/ChattingScreen.js
+++ src/screens/main/chat/ChattingScreen.js
@@ -2,8 +2,10 @@ import {SafeAreaView, ScrollView, View, TextInput} from "react-native";
 import ChatListTemplate from "@templates/ChatListTemplate";
 import SendTemplate from "@templates/Chatting/SendTemplate";
 import ReceiveTemplate from "@templates/Chatting/ReceiveTemplate";
+import {useState} from "react";
 
 const ChattingScreen = () => {
+    const [ChatInputHeight, setChatInputHeight] = useState(70);
     return (
         <SafeAreaView style={{
             backgroundColor: "white",
@@ -13,7 +15,7 @@ const ChattingScreen = () => {
             paddingRight: 10
         }}>
             <ScrollView style={{
-                height : "80%",
+                height: "80%",
                 width: "100%",
             }}>
                 <SendTemplate/>
@@ -49,23 +51,33 @@ const ChattingScreen = () => {
                 <SendTemplate/>
                 <ReceiveTemplate/>
             </ScrollView>
-<View>
-    <TextInput
-        style={{
-            borderRadius : 45,
-            borderWidth  : 1,
-            padding: 5,
-            paddingLeft : 20,
-            width : '95%',
-            marginLeft : 10,
-        }}
-        scrollEnabled = {true}
-        multiline={true}
-        autoCapitalize={'none'}
-        autoComplete = "off"
-        placeholder="useless placeholder"
-    />
-</View>
+            <View>
+                <TextInput
+                    style={{
+                        borderRadius: 25,
+                        borderWidth: 1,
+                        padding: 10,
+                        paddingLeft: 20,
+                        paddingRight: 20,
+                        width: '95%',
+                        marginLeft: 10,
+                        flex: 0,
+                        height: ChatInputHeight,
+                        fontSize: 15
+                    }}
+                    onContentSizeChange={(event) => {
+                        const limitHeight = 150;
+                        const contentHeight = event.nativeEvent.contentSize.height;
+                        const isExceed = limitHeight < contentHeight;
+                        setChatInputHeight(isExceed ? limitHeight : event.nativeEvent.contentSize.height)
+                    }}
+                    numberOfLines={5}
+                    multiline={true}
+                    autoCapitalize={'none'}
+                    autoComplete="off"
+                    placeholder="Message..."
+                />
+            </View>
         </SafeAreaView>
     )
 }
