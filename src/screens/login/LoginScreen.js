0fa810d2e46011af33ae59392e6c3ef86177f240
임석현 <sjsh1623@flow.team>
2022-07-27 00:00:16 +0900

    - Add Button - Login

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -1,16 +1,34 @@
-import {View, SafeAreaView, StyleSheet, TextInput} from "react-native";
+import {View, Text, SafeAreaView, StyleSheet, TextInput, TouchableOpacity} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import React from "react";
 
 const LoginScreen = () => {
     return (
         <SafeAreaView style={style.container}>
-            <TextInput
-                style={style.textInput}
-                placeholder="useless placeholder"
-            />
-            <TextInput
-                style={style.textInput}
-                placeholder="useless placeholder"
-            />
+            <View style={style.textContainer}>
+                <TextInput
+                    style={style.textInput}
+                    placeholder="ID"
+                />
+                <TextInput
+                    style={style.textInput}
+                    placeholder="Password"
+                />
+            </View>
+            <View style={style.buttonContainer}>
+                <TouchableOpacity style={style.button}>
+                    <Text style={{
+                        color: 'white',
+                        fontWeight: 'bold'
+                    }}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={style.button}>
+                    <Text style={{
+                        color: 'white',
+                        fontWeight: 'bold'
+                    }}>Create account</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -22,7 +40,35 @@ const style = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    textInput: {}
+    textInput: {
+        width: '70%',
+        paddingTop: 7,
+        paddingBottom: 5,
+        paddingLeft: 10,
+        borderBottomWidth: 1,
+        fontSize: 15,
+    },
+    textContainer: {
+        width : '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        width : '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop : '7%'
+    },
+    button: {
+        marginBottom: 10,
+        width: '60%',
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 45,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 })
 
 export default LoginScreen;
